5f88582910a01a860e6addca8fffed55e4fd4d32
Mohamed Khalil <mohamed.khalil.ucbl@gmail.com>
2025-11-22 21:28:43 +0100

    new project

--- url-shortener/README.md
+++ url-shortener/README.md
@@ -1,4 +1,4 @@
-# UlShortner
+# UrlShortener
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 20.3.2.
 
--- url-shortener/src/app/app.ts
+++ url-shortener/src/app/app.ts
@@ -2,9 +2,10 @@ import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('ul-shortner');
+  protected readonly title = signal('url-shortener');
 }
